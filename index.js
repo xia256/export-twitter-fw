27fc60dbdec55ca2f1e26079f9d82ff0226461fb
xia256 <asphyxia@novusphere.io>
2021-01-11 21:11:56 -0500

    support multiple targets move to config.json

--- index.js
+++ index.js
@@ -2,36 +2,31 @@
 const Twitter = require('twitter');
 const fs = require('fs');
 
-const config = {
-    target: "thenovusphere", // the account name we're targeting
-    delay: 0, // if you have more than 50,000 followers this should be set to true
-    //
-    // go to: https://developer.twitter.com/en/portal/projects-and-apps
-    // make a standalone app
-    // go into keys and tokens
-    //
-    twitter: {
-        "consumer_key": "", // api key
-        "consumer_secret": "", // api key secret
-        "access_token_key": "", // authentication token key
-        "access_token_secret": "" // authentication token secret
+function delay(timeout) {
+    timeout = Math.max(timeout, 1);
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+}
+
+function readTextFile(path, defaultResult) {
+    if (!fs.existsSync(path)) {
+        return defaultResult;
     }
-};
+    return fs.readFileSync(path, 'utf8');
+}
 
 (async function main() {
 
     try {
 
-        if (Object.values(config.twitter).some(s => !s)) {
+        const config = JSON.parse(readTextFile('config.json', '{}'));
+        const history = JSON.parse(readTextFile('history.json', '{}'));
+
+        if (!config.twitter || Object.values(config.twitter).some(s => !s)) {
             throw new Error(`config.twitter has not been configured`);
         }
 
         const client = new Twitter(config.twitter);
-
-        function delay(timeout) {
-            timeout = Math.max(timeout, 1);
-            return new Promise((resolve) => setTimeout(resolve, timeout));
-        }
+        const refreshTarget = config.refreshTargetHours * 60 * 60 * 1000;
 
         async function getUsers(delayMs, getUsersAsync) {
             let cursor = undefined;
@@ -40,7 +35,7 @@ const config = {
             do {
                 const users = await getUsersAsync(cursor);
 
-                ids.push(...users.ids);
+                ids.push(...users.ids.map(id => id.toString()));
                 cursor = users.next_cursor;
 
                 console.log(`Currently have ${ids.length} ids collected, more=${cursor ? true : false}, waiting ${delayMs}ms`);
@@ -61,38 +56,69 @@ const config = {
                 }
             }
 
-            return ids.map(id => map[id]);
+            return ids.map(id => map[id]).filter(user => user);
         }
 
-        let [{ screen_name, id_str }] = (await client.get('users/lookup', {
-            screen_name: config.target
-        }));
+        const out_following = ['id,screen_name,following_id,following_sn'];
+        const out_followers = ['id,screen_name,following_id,following_sn'];
+        const all_users = ['id,screen_name'];
+        let all_map = {};
 
-        const followers = await getUsers(config.delay, async (cursor) => await client.get('followers/ids', {
-            screen_name: screen_name,
-            stringify_ids: true,
-            cursor
-        }));
+        for (const target of config.targets) {
+            console.log(`Working on target=${target}`);
+            const now = Date.now();
 
-        const following = await getUsers(config.delay, async (cursor) => await client.get('friends/ids', {
-            screen_name: screen_name,
-            stringify_ids: true,
-            cursor
-        }));
+            let [{ screen_name, id_str }] = (await client.get('users/lookup', {
+                screen_name: target
+            }));
 
-        const allMap = [{ id_str, screen_name }, ...followers, ...following].reduce((acc, user) => (acc[user.id_str] = user, acc), {});
+            let followers, following;
 
-        const out_following = following.map(user => `${id_str},@${screen_name},${user.id_str},@${user.screen_name}`);
-        const out_followers = followers.map(user => `${user.id_str},@${user.screen_name},${id_str},@${screen_name}`);
-        const all_users = Object.values(allMap).map(user => `${user.id_str},@${user.screen_name}`);
+            if (!history[id_str] || (now - history[id_str].time) >= refreshTarget) {
+                console.log(`Need to refresh data on target=${target}`);
 
-        out_following.unshift('id,screen_name,following_id,following_sn');
-        out_followers.unshift('id,screen_name,following_id,following_sn');
-        all_users.unshift('id,screen_name');
+                followers = await getUsers(config.delay, async (cursor) => await client.get('followers/ids', {
+                    screen_name: screen_name,
+                    stringify_ids: true,
+                    cursor
+                }));
 
-        fs.writeFileSync('following.csv', out_following.join('\r\n'), 'utf8');
-        fs.writeFileSync('followers.csv', out_followers.join('\r\n'), 'utf8');
-        fs.writeFileSync('nodes.csv', all_users.join('\r\n'), 'utf8');
+                following = await getUsers(config.delay, async (cursor) => await client.get('friends/ids', {
+                    screen_name: screen_name,
+                    stringify_ids: true,
+                    cursor
+                }));
+
+                history[id_str] = {
+                    screen_name,
+                    time: now
+                };
+
+                fs.writeFileSync(`data/history/${id_str}.followers.json`, JSON.stringify(followers), 'utf8');
+                fs.writeFileSync(`data/history/${id_str}.following.json`, JSON.stringify(following), 'utf8');
+                fs.writeFileSync('history.json', JSON.stringify(history), 'utf8');
+            }
+            else {
+                console.log(`We have history on target=${target}, so not pulling new data`);
+
+                followers = JSON.parse(readTextFile(`data/history/${id_str}.followers.json`));
+                following = JSON.parse(readTextFile(`data/history/${id_str}.following.json`));
+            }
+
+            const $all_map = [{ id_str, screen_name }, ...followers, ...following].reduce((acc, user) => (acc[user.id_str] = user, acc), all_map);
+            const $out_following = following.map(user => `${id_str},@${screen_name},${user.id_str},@${user.screen_name}`);
+            const $out_followers = followers.map(user => `${user.id_str},@${user.screen_name},${id_str},@${screen_name}`);
+
+            out_following.push(...$out_following);
+            out_followers.push(...$out_followers);
+        }
+
+        all_users.push(...Object.values(all_map).map(user => `${user.id_str},@${user.screen_name}`));
+
+        fs.writeFileSync('data/following.csv', Array.from(new Set(out_following)).join('\r\n'), 'utf8');
+        fs.writeFileSync('data/followers.csv', Array.from(new Set(out_followers)).join('\r\n'), 'utf8');
+        fs.writeFileSync('data/nodes.csv', all_users.join('\r\n'), 'utf8');
+        fs.writeFileSync('history.json', JSON.stringify(history, null, 2), 'utf8');
 
     }
     catch (ex) {
